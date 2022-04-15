8d21976fd9f8236b272624ad192e0d3a617cefa5
Saulo Gil <80283365+saulosgil@users.noreply.github.com>
2022-04-15 11:19:15 -0300

    Add files via upload

---
+++ Curso - Data Science - Agit/aula 1/noir_aula1__arq02_script.py
@@ -0,0 +1,7 @@
+from time import sleep
+print("Mathema Code", end='\r')
+sleep(2)
+print("orgulhosamente apresenta", end='\r')
+sleep(2)
+print("Theory Of Stevens: Os 4 Tipos de Variáveis", end='\r')
+sleep(3)
